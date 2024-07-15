dea46074db3e01233f02d564976817821653c1ab
wdndev <dongnian.wang@outlook.com>
2024-07-15 23:36:24 +0800

    base model

---
+++ tiny_rag/embedding/base.py
@@ -0,0 +1,23 @@
+import os
+from typing import Dict, List, Optional, Tuple, Union
+from numpy as np
+
+class BaseEmbeddings:
+    """ Base class for embeddings
+    """
+    def __init__(self, path: str, is_api: bool) -> None:
+        self.path = path
+        self.is_api = is_api
+
+    def get_embedding(self, texts: List[str]) -> List[float]:
+        raise NotImplemented
+    
+    @classmethod
+    def cosine_similarity(cls, vector1: List[float], vector2: List[float]) -> float:
+        """ calculate cosine similarity between two vectors
+        """
+        dot_product = np.dot(vector1, vector2)
+        magntude = np.linalg.norm(vector1) * np.linalg.norm(vector2)
+        if not magntude:
+            return 0
+        return dot_product / magntude
