c14a37281c24b7dd7357d1edc2c5fa37feb04d48
wdndev <dongnian.wang@outlook.com>
2024-08-02 22:55:26 +0800

    parser

--- test/parser_test.py
+++ test/parser_test.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append(".")
+
 import os
 
 from tinyrag import BaseParser
--- tinyrag/sentence_splitter.py
+++ tinyrag/sentence_splitter.py
@@ -6,13 +6,15 @@ from modelscope.pipelines import pipeline
 
 class SentenceSplitter:
     """ 句子分割模型
-        use_model: 指定是否用语义切分文档, 采取的文档语义分割模型为nlp_bert_document-segmentation_chinese-base，论文见https://arxiv.org/abs/2107.09278
+        use_model: 指定是否用语义切分文档, 采取的文档语义分割模型为 nlp_bert_document-segmentation_chinese-base， 论文见https://arxiv.org/abs/2107.09278
     """
     def __init__(self, 
                  use_model: bool = False, 
+                 min_sent_len = 100,
                  model_path: str = "damo/nlp_bert_document-segmentation_chinese-base", 
                  device="cpu"
         ):
+        self.min_sent_len = min_sent_len
         self.use_model = use_model
         if self.use_model:
             # assert model_path == "" "模型路径为空"
@@ -36,4 +38,18 @@ class SentenceSplitter:
                     sent_list[-1] += ele
                 elif ele:
                     sent_list.append(ele)
-        return sent_list
+
+        # 合并长度不足的句子
+        merged_sent_list = []
+        curr_sentence = ''
+        for sent in sent_list:
+            if len(curr_sentence) + len(sent) < self.min_sent_len:
+                curr_sentence += sent
+            else:
+                if curr_sentence:
+                    merged_sent_list.append(curr_sentence)
+                curr_sentence = sent
+        if curr_sentence:  # 添加最后一个句子
+            merged_sent_list.append(curr_sentence)
+
+        return merged_sent_list
